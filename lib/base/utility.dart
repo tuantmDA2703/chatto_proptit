b27b971cb2da112a666b3599899a9e848494110e
Minh Tuan Tran <minhtuantran270399@gmail.com>
2022-03-13 22:48:06 +0700

    feature(BoilerPlate/state-management) As a developer, i want to create the base screen

--- lib/base/utility.dart
+++ lib/base/utility.dart
@@ -3,4 +3,14 @@ import 'package:get_it/get_it.dart';
 
 class Utility{
   var navigator = GetIt.I<NavigatorService>();
+
+  void logData(String key,String message){
+    // ignore: avoid_print
+    print('==>$key:$message');
+  }
+
+  DateTime getCurrentTime(){
+    DateTime now = DateTime.now();
+    return now;
+  }
 }
