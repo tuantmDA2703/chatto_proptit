a4c87ff29dd2ce616b6c9736cf188563210dc19a
Tris1702 <phuonghoand2001@gmail.com>
2022-03-13 22:10:04 +0700

    features/ca 03 creating base sharedpreference service

--- lib/base/set_up_service_locator.dart
+++ lib/base/set_up_service_locator.dart
@@ -1,5 +1,7 @@
+import 'package:chatto_app/services/app_shared_preference.dart';
 import 'package:get_it/get_it.dart';
 
-void setUpServiceLocator(){
+void setUpServiceLocator() {
   GetIt getIt = GetIt.I;
+  getIt.registerLazySingleton<AppSharedPreference>(() => AppSharedPreference());
 }
