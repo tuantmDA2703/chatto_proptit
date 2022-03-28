52047f8ed1431e88a4fdcec51151e602ca58a59f
Tris1702 <phuonghoand2001@gmail.com>
2022-03-28 19:24:22 +0700

    [CA-13] feature(onboard/splash)As an user, i want to crete splash screen

--- lib/screen/splash/splash_page.dart
+++ lib/screen/splash/splash_page.dart
@@ -3,6 +3,8 @@ import 'package:chatto_app/base/utility.dart';
 import 'package:chatto_app/config/app_route.dart';
 import 'package:chatto_app/config/app_string.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -15,12 +17,36 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     Utility utility = Utility();
-    Timer(const Duration(seconds: 2), () async {
-      utility.navigator.pushed(AppRoute.login);
-    });
+    Timer(
+      const Duration(seconds: 2),
+      () => utility.navigator.popAndPush(AppRoute.login),
+    );
     return Scaffold(
       body: Center(
-        child: Text('Splash ${AppString().str}'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 100.0),
+              child: Column(
+                children: [
+                  SvgPicture.asset('assets/icons/ic_splash.svg'),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 50.0),
+                    child: Text(
+                      AppString.welcomeTitle,
+                      style: TextStyle(
+                        fontSize: 32,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //Welcome title
+          ],
+        ),
       ),
     );
   }
